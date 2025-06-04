3959b8154ed8a14d34f949036ced4ab0f647718c
chashimoto7 <christinehashimoto7@gmail.com>
2025-06-04 10:51:16 -0700

    Update Layout to history

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -39,7 +39,7 @@ export default function Layout() {
             </NavLink>
 
             <NavLink
-              to="/journal-history"
+              to="/history"
               className={({ isActive }) =>
                 `flex items-center gap-2 px-4 py-2 rounded-md font-medium transition ${
                   isActive
