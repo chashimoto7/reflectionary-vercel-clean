54f9ec870fa45a5a8a368a067d6c4550f7862e26
chashimoto7 <christinehashimoto7@gmail.com>
2025-06-04 15:02:17 -0700

    Update NewEntry HandleFollowUp

--- src/pages/NewEntry.jsx
+++ src/pages/NewEntry.jsx
@@ -314,7 +314,7 @@ export default function NewEntry() {
         {
           method: "POST",
           headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ user_id: userId }),
+          body: JSON.stringify({ user_id: entryChain }),
         }
       );
 
