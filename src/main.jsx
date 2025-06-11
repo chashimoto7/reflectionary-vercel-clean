4daaba4fb89b1caa29455fc485470ef423c0541f
chashimoto7 <christinehashimoto7@gmail.com>
2025-06-11 13:22:53 -0700

    Update vercel.json and add /public/index.html

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
-// src/main.jsx (or index.js)
+// src/main.jsx
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+
+// Import IBM Plex Sans font
+import "@fontsource/ibm-plex-sans/400.css";
+import "@fontsource/ibm-plex-sans/500.css";
+import "@fontsource/ibm-plex-sans/600.css";
+import "@fontsource/ibm-plex-sans/700.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
