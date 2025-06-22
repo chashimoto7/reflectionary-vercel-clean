4059248e045a44cf99860e7b1d9fc84af43dfa64
chashimoto7 <christinehashimoto7@gmail.com>
2025-06-21 20:54:10 -0700

    Force vercel redeploy

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,3 +1,4 @@
+//src/pages/welcome
 import logo from "../assets/ReflectionaryWordWelcome.png";
 import squarelogo from "../assets/FinalReflectionarySquare.png";
 import React, { useState, useEffect } from "react";
