69393313616f48c0f174ee4404589c825b0ba66e
chashimoto7 <christinehashimoto7@gmail.com>
2025-06-28 16:24:30 -0700

    Layout background

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -136,7 +136,7 @@ export default function Layout({ children }) {
   }
 
   return (
-    <div className="flex min-h-screen overflow-hidden relative bg-gradient-to-r from-slate-900 via-purple-800 to-orange-900">
+    <div className="flex min-h-screen overflow-hidden relative bg-gradient-to-r from-slate-900 via-purple-800 to-fuchsia-900">
       {/* Sidebar - Hidden on Welcome page */}
       {!isWelcomePage && (
         <aside className="relative w-72 z-10">
